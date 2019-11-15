a985783a20ea6609f1d98bab51e901dc1bd1fb08
Frank Martin <cedricmrtn@gmail.com>
2019-11-15 15:39:34 -0700

    Initial Repository

---
+++ src/MakeChangeApp.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MakeChangeApp {
+
+	public static void main(String[] args) {
+			Scanner kb = new Scanner(System.in);
+			double twenties;
+			double tens;
+			double fives;
+			double ones;
+			double quarters;
+			double dimes;
+			double nickels; 
+			double pennies;		
+			
+			System.out.println("What was the price of your item? ");
+			double itemPrice = kb.nextDouble();
+
+	}
+
+}
