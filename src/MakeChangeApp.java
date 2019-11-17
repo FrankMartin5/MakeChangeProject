51f523fc60ab869558aac3537c176b9ad2f0c2f2
Frank Martin <cedricmrtn@gmail.com>
2019-11-17 12:46:54 -0700

    3 new variables declared and ifelse statements made

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -4,18 +4,25 @@ public class MakeChangeApp {
 
 	public static void main(String[] args) {
 			Scanner kb = new Scanner(System.in);
-			double twenties;
-			double tens;
-			double fives;
-			double ones;
-			double quarters;
-			double dimes;
-			double nickels; 
-			double pennies;		
+			double itemPrice = 0, amountPaid = 0;
+			double twenties = 20;
+			double tens = 10;
+			double fives = 5;
+			double ones = 1;
+			double quarters = 0.25;
+			double dimes = 0.10;
+			double nickels = 0.05; 
+			double pennies = 0.01;	
+			
 			
 			System.out.println("What was the price of your item? ");
-			double itemPrice = kb.nextDouble();
+			itemPrice = kb.nextDouble();
+			
+			System.out.println("How much are you paying?");
+			amountPaid = kb.nextDouble();
+				
+			}
 
 	}
 
-}
+
